a757164287c86209bd7607208e3e030a3560ac92
Tofan <tofan.rukmana@bankina.id>
2022-10-08 19:40:00 +0700

    test

---
+++ README.md
@@ -0,0 +1,10 @@
+Tes FE Suitmedia
+list blog with paginations
+
+In the project directory, you can run:
+
+### `npm install`
+
+### `npm start`
+
+saya pakai image dumy karena di API tidak ada data image
---
+++ src/components/Posts.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../components/logo.png'; // with import
+
+const Posts = ({ posts, loading, totalPosts }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+    <p>showing 1 - 10 of {totalPosts}</p>
+    <ul className='list-group mb-4'>
+      {posts.map(post => (
+        <li key={post.id} className='list-group-item'>
+          <img src={logo}></img>
+          {post.title}
+        </li>
+      ))}
+    </ul>
+    </div>
+    
+  );
+};
+
+export default Posts;
